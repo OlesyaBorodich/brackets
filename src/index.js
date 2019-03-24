4905d9f9196d2b0399e0046d17763c1162fc2aac
OlesyaBorodich <lesyaborodich@mail.ru>
2019-03-24 21:08:51 +0300

    feat: add task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+    module.exports = function check(str, bracketsConfig) {
+  if (str.trim().length===0 || bracketsConfig.length===0) return false; 
+  var count = [];
+  var obj = {};
+  for (var i = 0; i < bracketsConfig.length; i++) {
+      if (bracketsConfig[i].length!==2) {
+          return false;
+      }
+  }
+  const arrStr = str.split('');
+  for (var j = 0; j < arrStr.length; j++) {
+      for (var i = 0; i < bracketsConfig.length; i++) {
+      if (arrStr[j]===bracketsConfig[i][0]) {
+                    if (obj[bracketsConfig[i][0]] == null) {
+                   count.push(bracketsConfig[i][0]);
+                  if (bracketsConfig[i][0]===bracketsConfig[i][1]) { 
+                    obj[bracketsConfig[i][0]] = true; 
+                  }
+              } 
+              else {
+                  delete obj[bracketsConfig[i][0]];
+              }
+           }
+          if (arrStr[j]===bracketsConfig[i][1]) {
+              if (obj[bracketsConfig[i][0]] == null) {
+                  if (count.length===0) return false;
+                   var pop = count.pop();
+                  if (pop !== bracketsConfig[i][0]) 
+                  return false;
+              }
+          }
+      }
+  }
+  return count.length===0;
 }
